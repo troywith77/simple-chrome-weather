271e5857a4510554349c56b006767a8ece486927
Lui <ruitang307@gmail.com>
2016-05-19 15:12:46 +0800

    refactor code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Simple Chrome Weather
+
+- [X] 通过城市名称查询天气
+- [X] 查询当前城市，打开扩展时自动查询
+
+## ScreenShots
+
+![crx](./images/ScreenShot2016-05-19at3.09.21PM.png)
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ajax
 function fetch(url, callback) {
 	var xhr = new XMLHttpRequest();
     xhr.open('GET',url,true);
@@ -9,32 +10,19 @@ function fetch(url, callback) {
 	xhr.send()
 }
 
-// form
-var form = document.querySelector('#form');
-var input = document.querySelector('#value');
-
-// content
-var city = document.querySelector('#city');
-var temp = document.querySelector('#temp');
-var cold = document.querySelector('#cold');
-var forecast = document.querySelector('#forecast');
-
-form.addEventListener('submit', function(e) {
-	e.preventDefault()
-	var value = input.value;
+function getWeather(value) {
 	fetch('http://wthrcdn.etouch.cn/weather_mini?city=' + value, function(data) {
 	    if(!data) return;
 	    data = JSON.parse(data);
 		forecast.innerHTML = '';
 	    console.log(data.data)
+	    forecastHeader.innerHTML = ''
 
 	    city.innerHTML = data.data.city;
 	    temp.innerHTML = data.data.wendu + ' °C';
 	    cold.innerHTML = data.data.ganmao;
 
-	    var forecastHeader = document.createElement('h2');
-	    forecastHeader.innerHTML= 'Forecast';
-	    forecast.parentNode.insertBefore(forecastHeader, forecast)
+	    forecastHeader.innerHTML = '预报'
 	    for(var i of data.data.forecast) {
 	    	var tempLi = document.createElement('li');
 	    	tempLi.innerHTML =
@@ -50,4 +38,32 @@ form.addEventListener('submit', function(e) {
 
 	    input.value = ''
 	});
+}
+
+var currentCity = null;
+
+fetch('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json', function(data) {
+	var currentCity = JSON.parse(data).city
+
+	getWeather(currentCity)
+})
+
+// form selector
+var form = document.querySelector('#form');
+var input = document.querySelector('#value');
+
+// content selector
+var city = document.querySelector('#city');
+var temp = document.querySelector('#temp');
+var cold = document.querySelector('#cold');
+var forecast = document.querySelector('#forecast');
+var forecastHeader = document.querySelector('#forecast-header');
+
+
+// event handler
+form.addEventListener('submit', function(e) {
+	e.preventDefault()
+	var value = input.value;
+
+	getWeather(value)
 })
